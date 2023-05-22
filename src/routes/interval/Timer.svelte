d252b16444de2cc8140166e68ccef1ecaeb6baec
mdellio3 <m.elliott92@yahoo.com>
2023-05-21 18:52:33 -0600

    feat: countdown ring around timer

--- src/routes/interval/Timer.svelte
+++ src/routes/interval/Timer.svelte
@@ -1,11 +1,12 @@
 <script>
 	import { editing, laps, work, rest } from './store';
-	let time = 0;
 	let counting = false;
 	let working = true;
 	let max = $work * 100;
 	let currentLap = 1;
 	let interval;
+	let time = max;
+
 	const sound = new Audio('./countdown.wav');
 	sound.load();
 	const soundReset = () => {
@@ -15,16 +16,15 @@
 	const startTimer = () => {
 		time = max;
 		interval = setInterval(() => {
+			time -= 1;
 			const sec = Math.floor((time % 6000) / 100);
 			if (working) {
-				if (sec === 0) {
+				if (time === 0) {
 					working = false;
 					time = max = $rest * 100;
-				} else {
-					time -= 1;
 				}
 			} else {
-				if (sec === 0) {
+				if (time === 0) {
 					if (currentLap === $laps) {
 						stopTimer();
 						return;
@@ -32,12 +32,10 @@
 					working = true;
 					time = max = $work * 100;
 					currentLap++;
-				} else {
-					time -= 1;
 				}
 			}
 
-			if (time === 405) {
+			if (time === 305) {
 				sound.play();
 			}
 		}, 10);
@@ -51,7 +49,7 @@
 	};
 
 	const resetTimer = () => {
-		time = 0;
+		time = max;
 		currentLap = 1;
 		soundReset();
 	};
@@ -74,8 +72,22 @@
 	};
 </script>
 
-<h2>LAP {currentLap} / {$laps}</h2>
-<h1>{formatTime(time)}</h1>
+<div class="container">
+	<svg viewBox="0 0 350 350" xmlns="http://www.w3.org/2000/svg">
+		<circle
+			r="150"
+			cx="175"
+			cy="175"
+			stroke="#ddeeff"
+			stroke-width="5"
+			fill="none"
+			stroke-dasharray={943}
+			stroke-dashoffset={943 * (1 - time / max)}
+		/>
+	</svg>
+	<h2>LAP {currentLap} / {$laps}</h2>
+	<h1>{formatTime(time)}</h1>
+</div>
 
 <span>
 	{#if !counting}
@@ -99,15 +111,32 @@
 		margin: 0;
 		background-color: #223344;
 	}
+	.container {
+		position: relative;
+		width: 350px;
+		height: 350px;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+	}
+	svg {
+		position: absolute;
+		width: 100%;
+		height: 100%;
+		transform: rotate(90deg) scaleX(-1);
+	}
 	h1 {
 		color: #ddeeff;
-		font-size: 5em;
+		font-size: 3em;
 		font-family: sans-serif;
+		margin: 0;
 	}
 	h2 {
 		color: #aabbcc;
 		font-size: 2em;
 		font-family: sans-serif;
+		margin: 0;
 	}
 	@media screen and (max-width: 480px) {
 		span {
